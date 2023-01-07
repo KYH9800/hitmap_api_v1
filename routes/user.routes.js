9d2b077c56b29b528af3589129efd20bc13efbbd
고윤혁 <sksgur3217@gmail.com>
2023-01-07 23:48:20 +0900

    :chart_with_upwards_trend: [fix] 파일 분리로 인한 경로 설정 수정, class 추가

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const UserController = require('../controllers/user.controller');
-const userController = new UserController();
+const SignupController = require('../controllers/signup.controller');
+const signupController = new SignupController();
+
+const LoginController = require('../controllers/login.controller');
+const loginController = new LoginController();
 
 const upload = require('../multer/awsMulterModules');
 
-// 회원가입
-router.post('/signup', upload.single('image'), userController.signup);
-
-// 로그인
-router.post('/login', userController.login);
+router
+  .post('/signup', upload.single('image'), signupController.signup) // 회원가입
+  .post('/login', loginController.login); // 로그인
 
 module.exports = router;
