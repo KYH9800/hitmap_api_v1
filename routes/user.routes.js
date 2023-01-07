5f186eeb62434740890ae245438af4e235d105f2
고윤혁 <sksgur3217@gmail.com>
2023-01-07 22:31:34 +0900

    :sparkles: [fix] multer middleware 라우터에 추가

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const userController = new UserController();
 
+const upload = require('../multer/awsMulterModules');
+
 // 회원가입
-router.post('/signup', userController.signup);
+router.post('/signup', upload.single('image'), userController.signup);
 
 // 로그인
 router.post('/login', userController.login);
