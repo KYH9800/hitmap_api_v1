f7362b69635d6ede9590e4d06ec6f9c865f52226
kyuhyung <vision8love@gmail.com>
2023-01-09 16:35:07 +0900

    :bug: [fix] 이미지 파일 다운로드 현상 수정

--- multer/awsMulterModules.js
+++ multer/awsMulterModules.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const AWS = require('aws-sdk');
 const multerS3 = require('multer-s3');
 const path = require('path');
-console.log('path: ', path);
 
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -14,6 +13,7 @@ module.exports = multer({
   storage: multerS3({
     s3: new AWS.S3(), 
     bucket: 'kidkyu',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     key(req, file, cb) {
 
       const filiname = path.basename(file.originalname).trim();
