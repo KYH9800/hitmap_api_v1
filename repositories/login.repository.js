f136191e2cda864e42c70c067da72cdde0c1c951
고윤혁 <sksgur3217@gmail.com>
2023-01-09 09:09:42 +0900

    :sparkles: [feat] 로그인 기능 구현(token 발급)

--- repositories/login.repository.js
+++ repositories/login.repository.js
@@ -1,1 +1,17 @@
-// TODO
+class LoginRepository {
+  constructor(UserModel) {
+    this.userModel = UserModel;
+  }
+
+  findByEmail = async (email) => {
+    const user = await this.userModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    return user;
+  };
+}
+
+module.exports = LoginRepository;
