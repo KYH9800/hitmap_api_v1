07975ed077e8a1af283d4f7a3fe3c36bd4e3ada6
고윤혁 <sksgur3217@gmail.com>
2023-01-09 22:11:23 +0900

    :sparkles: [feat] 이메일 중복확인 router 추가

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,12 +7,16 @@ const signupController = new SignupController();
 const LoginController = require('../controllers/login.controller');
 const loginController = new LoginController();
 
+const CheckController = require('../controllers/check.controller');
+const checkController = new CheckController();
+
 const upload = require('../multer/awsMulterModules');
 const auth = require('../middlewares/auth-middleware');
 
 router
   .post('/signup', auth.isNotLoggedIn, upload.single('image'), signupController.signup) // 회원가입
   .post('/login', auth.isNotLoggedIn, loginController.login) // 로그인
-  .post('/logout', auth.isLoggedIn, loginController.logout); // 로그아웃
+  .post('/logout', auth.isLoggedIn, loginController.logout) // 로그아웃
+  .get('/email', checkController.findByEmail); // 이메일 중복확인
 
 module.exports = router;
