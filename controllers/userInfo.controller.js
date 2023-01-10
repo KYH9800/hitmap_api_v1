97eada837917d256b7f5d5273fa609fdfdc61e11
고윤혁 <sksgur3217@gmail.com>
2023-01-11 00:03:22 +0900

    :sparkles: [feat] 내 정보 수정 기능 추가

--- controllers/userInfo.controller.js
+++ controllers/userInfo.controller.js
@@ -1,6 +1,7 @@
-const { get_my_info } = require('../services/userInfo.service');
+const { get_my_info, update_user_info } = require('../services/userInfo.service');
 
 class UserInfoController {
+  // 내 정보 조회
   getMyInfo = async (req, res) => {
     try {
       const { user_id } = res.locals;
@@ -24,6 +25,33 @@ class UserInfoController {
       }
       return res.status(400).send({
         errorMessage: '내 정보 조회에 실패하였습니다.',
+      });
+    }
+  };
+
+  // 내 정보 수정
+  updateUserInfo = async (req, res) => {
+    try {
+      // const { user_id } = res.locals;
+      const user_id = 17;
+      const image = req.file?.location;
+      const { prevPassword, password, passwordConfirm, nickname } = req.body;
+
+      await update_user_info(user_id, prevPassword, password, passwordConfirm, nickname, image);
+
+      return res.status(201).send({
+        message: '회원정보 수정 완료',
+      });
+    } catch (error) {
+      console.log(error);
+      if (error.message) {
+        return res.status(error.statusCode).send({
+          errorMessage: error.message,
+          status: error.statusCode,
+        });
+      }
+      return res.status(400).send({
+        errorMessage: '회원정보 수정에 실패하였습니다.',
       });
     }
   };
--- routes/userInfo.routes.js
+++ routes/userInfo.routes.js
@@ -5,7 +5,9 @@ const UserInfoController = require('../controllers/userInfo.controller');
 const userInfoController = new UserInfoController();
 
 const auth = require('../middlewares/auth-middleware');
+const upload = require('../multer/awsMulterModules');
 
-router.get('/', auth.isLoggedIn_refresh_token, userInfoController.getMyInfo); // 내 정보 조회
-
+router
+  .get('/', auth.isLoggedIn_refresh_token, userInfoController.getMyInfo) // 내 정보 조회
+  .patch('/', upload.single('image'), userInfoController.updateUserInfo); // 내 정보 수정
 module.exports = router;
