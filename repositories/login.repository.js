d27a5abec350519b5ff5feaf734dd62dadee3761
고윤혁 <sksgur3217@gmail.com>
2023-01-10 16:32:32 +0900

    :bug: [fix] 로그인 시 유저 정보 없다고 알림

--- repositories/login.repository.js
+++ repositories/login.repository.js
@@ -1,3 +1,5 @@
+const { CustomError } = require('../utils/Error');
+
 class LoginRepository {
   constructor(UserModel) {
     this.userModel = UserModel;
@@ -9,6 +11,10 @@ class LoginRepository {
         email: email,
       },
     });
+
+    if (!user) {
+      throw new CustomError('이메일 또는 비밀번호를 확인해주세요.', 412);
+    }
 
     return user;
   };
