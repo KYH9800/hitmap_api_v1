721c053f9bd8be6bed2f10ce8a42f2443c95eeb4
고윤혁 <sksgur3217@gmail.com>
2023-01-11 17:45:09 +0900

    :hammer: [fix] open api url 한글 설정

--- weatherAPI/services/weatherAPI.service.js
+++ weatherAPI/services/weatherAPI.service.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 // open weather: 풍향, 풍속, 기온
 const get_weather = async (lat, lon) => {
-  const open_weather_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEYS}`;
+  const open_weather_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${process.env.OPEN_WEATHER_API_KEYS}`;
   const data = await axios
     .get(open_weather_API_URL)
     .then((response) => {
