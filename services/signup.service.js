62ad9ff90f5d1bd12bd8935aac97f2c7ec3532cc
고윤혁 <sksgur3217@gmail.com>
2023-01-20 13:56:00 +0900

    :bug: [fix} 회원가입 이미지 URL 주소 수정

--- services/signup.service.js
+++ services/signup.service.js
@@ -29,7 +29,7 @@ const user_register = async (email, password, passwordConfirm, nickname, image) 
 
   if (!image) {
     if (process.env.NODE_ENV === 'production') {
-      image = 'http://https://koyunhyeok.shop/userImage/images/user_logo.png';
+      image = 'https://koyunhyeok.shop/userImage/images/user_logo.png';
     } else {
       image = 'http://localhost:3065/userImage/images/user_logo.png';
     }
