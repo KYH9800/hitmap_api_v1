96942bbb885dc3c37759cd07a66ddb67318cce86
고윤혁 <sksgur3217@gmail.com>
2023-01-26 21:48:38 +0900

    :card_file_box: [fix] User Table 비밀번호 Null값 허용, 소셜로그인 관련 테이블 제거

--- models/User.js
+++ models/User.js
@@ -5,8 +5,6 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       this.hasOne(models.UserImage, { foreignKey: 'user_id' });
-      this.hasOne(models.KaKaoUser, { foreignKey: 'user_id' });
-      this.hasOne(models.NaverUser, { foreignKey: 'user_id' });
       this.hasMany(models.Post, { foreignKey: 'user_id' });
       this.hasMany(models.Comment, { foreignKey: 'user_id' });
       this.hasMany(models.Like, { foreignKey: 'user_id' });
@@ -29,12 +27,16 @@ module.exports = (sequelize, DataTypes) => {
       },
       password: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       nickname: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+      },
+      social: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       createdAt: {
         type: DataTypes.DATE,
