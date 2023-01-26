9920d9868b5ec657a8299f327dd71ec42197f955
고윤혁 <sksgur3217@gmail.com>
2023-01-26 21:47:13 +0900

    :sparkles: [feat] 카카오 로그인 기능 추가, 카카오 로그인 user에 따른 기능 분기 처리

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -18,6 +18,8 @@ router
   .post('/login', auth.is_not_logged_in, loginController.login) // 로그인
   .post('/logout', auth.is_logged_in, loginController.logout) // 로그아웃
   .post('/email', auth.is_not_logged_in, checkController.findByEmail) // 이메일 중복확인
-  .post('/nickname', auth.is_not_logged_in, checkController.findByNickname); // 닉네임 중복확인
+  .post('/nickname', auth.is_not_logged_in, checkController.findByNickname) // 닉네임 중복확인
+  .get('/kakaoLogin/start', auth.is_not_logged_in, loginController.startKakaoLogin) // 카카오 로그인
+  .post('/kakaoLogout', auth.is_logged_in, loginController.kakaoLogout); // 카카오 로그아웃
 
 module.exports = router;
