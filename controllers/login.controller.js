15a29d4b5e3d7f5bbb878ceaca6b725aeb7e2e69
고윤혁 <sksgur3217@gmail.com>
2023-01-28 12:44:17 +0900

    :recycle: [fix] 카카오로그인 기능 수정

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -67,7 +67,7 @@ class LoginController {
   // 카카오 로그인
   startKakaoLogin = async (req, res) => {
     try {
-      const { code } = req.query;
+      const { code } = req.body;
       console.log('code: ', code);
 
       // token 발급
@@ -84,14 +84,19 @@ class LoginController {
       console.log('nickname: ', nickname);
 
       if (process.env.NODE_ENV === 'production') {
-        // 배포 시 메인 홈 화면으로 리다이렉트
-        return res.redirect('https://hitmap-fe.vercel.app');
+        res.cookie('access_token', access_token, { sameSite: 'None', secure: false, httpOnly: true });
+        res.cookie('refresh_token', refresh_token, { sameSite: 'None', secure: false, httpOnly: true });
       } else {
-        // 개발할 때는 로컬에서의 테스트 화면
-        return res.redirect(
-          `http://localhost:3000/?access_token=${access_token}&refresh_token=${refresh_token}&nickname=${nickname}`,
-        );
+        res.cookie('access_token', access_token);
+        res.cookie('refresh_token', refresh_token);
       }
+
+      return res.status(200).send({
+        message: '로그인 성공',
+        access_token: access_token,
+        refresh_token: refresh_token,
+        nickname: nickname,
+      });
     } catch (error) {
       if (error.message) {
         console.log('error: ', error);
@@ -103,42 +108,6 @@ class LoginController {
       }
       return res.status(400).send({
         errorMessage: '로그아웃에 실패하였습니다.',
-      });
-    }
-  };
-
-  // 카카오 로그인 jwt 토큰 생성
-  make_jwt_using_kakao_token = async (req, res) => {
-    try {
-      const { access_token, refresh_token, nickname } = req.body;
-      console.log('access_token: ', access_token);
-      console.log('refresh_token: ', refresh_token);
-      console.log('nickname: ', nickname);
-
-      if (process.env.NODE_ENV === 'production') {
-        res.cookie('access_token', access_token, { sameSite: 'None', secure: false, httpOnly: true });
-        res.cookie('refresh_token', refresh_token, { sameSite: 'None', secure: false, httpOnly: true });
-      } else {
-        res.cookie('access_token', access_token);
-        res.cookie('refresh_token', refresh_token);
-      }
-
-      res.status(200).send({
-        message: '로그인 성공',
-        access_token: access_token,
-        refresh_token: refresh_token,
-        nickname: nickname,
-      });
-    } catch (error) {
-      console.log(error);
-      if (error.message) {
-        return res.status(error.statusCode).send({
-          errorMessage: error.message,
-          status: error.statusCode,
-        });
-      }
-      return res.status(400).send({
-        errorMessage: '로그인에 실패하였습니다.',
       });
     }
   };
