b49066783ad98684a11c8d1fb598ef39e3cf7738
고윤혁 <sksgur3217@gmail.com>
2023-02-04 02:22:24 +0900

    :sparkles: [feat] 채팅 Room 생성 기능 추가

--- services/socket.service.js
+++ services/socket.service.js
@@ -2,6 +2,39 @@ const Room = require('../schemas/room');
 const Chat = require('../schemas/chat');
 
 class SocketService {
+  createRoom = async (roomId, userId, guestId, userImage, userName, content) => {
+    console.log('roomId: ', roomId);
+    console.log('userId: ', userId);
+    console.log('guestId: ', guestId);
+    console.log('userImage: ', userImage);
+    console.log('userName: ', userName);
+    console.log('content: ', content);
+
+    const room = Room.create({
+      roomId: roomId,
+      userId: userId,
+      guestId: guestId,
+      userImage: userImage,
+      userName: userName,
+      content: content,
+    });
+    console.log('room: ', room);
+  };
+
+  findMyRooms = async (userId) => {
+    const rooms = Room.find({
+      roomId: userId,
+    });
+
+    return rooms;
+  };
+
+  findRoom = async (roomId) => {
+    const findRoom = Room.find({ roomId: roomId });
+
+    return findRoom;
+  };
+
   removeRoom = async (roomId) => {
     try {
       await Room.deleteOne({ _id: roomId });
