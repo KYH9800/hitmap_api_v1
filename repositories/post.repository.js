80cfb7922ec5c85b7a2ad42a2e9bd4d84f07115b
kyuhyung <vision8love@gmail.com>
2023-02-04 02:14:41 +0900

    :bulb: [fix] 무한 스크롤 일시적 주석

--- repositories/post.repository.js
+++ repositories/post.repository.js
@@ -1,4 +1,4 @@
-const { Op } = require('sequelize');
+// const { Op } = require('sequelize');
 
 class PostRepository {
   constructor(UserModel, UserImageModel, PostModel, PostImageModel, FishInfoModel, CommentModel, LikeModel) {
